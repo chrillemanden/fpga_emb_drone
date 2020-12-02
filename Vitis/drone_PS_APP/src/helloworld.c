c76b8bcf9f9b7272ed9682273ad9e0b86915e4dd
Soeren Madsen <soren.madsen97@gmail.com>
2020-12-02 14:48:17 +0100

    Gyro angle work in progress
    Started creating a function to calculate the angle from the gyroscope

--- Vitis/drone_PS_APP/src/helloworld.c
+++ Vitis/drone_PS_APP/src/helloworld.c
@@ -16,8 +16,10 @@
 #include "xparameters.h"
 #include "xbram.h"
 #include "sleep.h"
+#include "time.h"
 
 #define MYMEM_u(A) ((volatile u32*)ConfigPtr->MemBaseAddress)[A]
+#define gyro_div 131
 //#define MAXMEM	   ((ConfigPtr->MemHighAddress-ConfigPtr->MemBaseAddress)+1)/sizeof(u32)
 
 XBram Bram;
@@ -33,24 +35,75 @@ int main()
 	xil_printf("BRAM initialization complete.\r\n");
 
 	int mem_value, addr_value=1;
+
 	int8_t signed_acc_data = 0;
+	int16_t gyro_raw_x, gyro_raw_y, gyro_raw_z, acc_raw_x, acc_raw_y, acc_raw_z, prev_gyro = 0;
+	float gyro_angle_x, gyro_angle_y = 0;
+	float gyro_raw_initial_x, gyro_raw_initial_y;
+
 
   while(1)
   {
 		  //xil_printf("Enter memory value for address %d:\r\n", addr_value);
 	  	  //scanf("%d", &mem_value);
 
-		  mem_value = MYMEM_u(addr_value);				//corresponds to memory address 4 on the FPGA (X"40000004")
+		  //mem_value = MYMEM_u(addr_value);				//corresponds to memory address 4 on the FPGA (X"40000004")
 
-		  signed_acc_data = mem_value;
-		  xil_printf("Memory value %d for addr %d.\r\n", signed_acc_data, addr_value);
-
-	  usleep(1000);
+		  //signed_acc_data = mem_value;
+		  //xil_printf("Memory value %d for addr %d.\r\n", signed_acc_data, addr_value);
+		acc_raw_x = MYMEM_u(0);
+		acc_raw_y = MYMEM_u(1);
+		acc_raw_z = MYMEM_u(2);
+		gyro_raw_x = MYMEM_u(3);
+		gyro_raw_y = MYMEM_u(4);
+		gyro_raw_z = MYMEM_u(5);
+		calc_gyro_ang(gyro_raw_x, gyro_raw_y, &gyro_angle_x, &gyro_angle_y);
+	  //usleep(1000);
 
   } //end while loop
 
     cleanup_platform();
     return 0;
+}
+
+void calc_gyro_ang(int16_t gyro_raw_x, int16_t gyro_raw_y, float* gyro_angle_x, float* gyro_angle_y)
+{
+	static float gyro_raw_initial_x, gyro_raw_initial_y = 0;
+	static int i, gyro_error = 0;
+	static float prev_gyro = gyro_raw_y;
+	struct timeval stop, start;
+	static float time_us = 0;
+	//Initial value for gyroscope
+	if (gyro_error == 0)
+	{
+		gyro_raw_initial_x = gyro_raw_x/gyro_div;
+		gyro_raw_initial_y = gyro_raw_y/gyro_div;
+		if (prev_gyro != gyro_raw_y)
+		{
+			if (i<199)
+			{
+				prev_gyro = gyro_raw_y;
+				gyro_raw_initial_x = gyro_raw_initial_x + gyro_raw_x/gyro_div;
+				gyro_raw_initial_y = gyro_raw_initial_y + gyro_raw_y/gyro_div;
+				i++;	
+			}
+			else if (i == 199)
+			{
+				gettimeofday(&start, NULL);
+				time_us = start.tv_sec*1000000+start.tv_usec;
+				gyro_raw_initial_x = (gyro_raw_initial_x + gyro_raw_x/gyro_div)/200;
+				gyro_raw_initial_y = (gyro_raw_initial_y + gyro_raw_y/gyro_div)/200;
+				gyro_error = 1;
+			}
+			
+		}
+	}
+	else
+	{
+
+		gyro_angle_x = gyro_angle_x + ((gyro_raw_x/gyro_div)-gyro_raw_initial_x)*elapsed_time;
+	}
+	
 }
 
 
